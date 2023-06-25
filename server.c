8d8e885fce8941914dc19b87c5b95deacb90f094
sioffm <137110958+sioffm@users.noreply.github.com>
2023-06-25 18:55:53 +0200

    Update server.c

--- server.c
+++ server.c
@@ -33,7 +33,7 @@ void message_loop(int connfd) //connfd connection file descriptor
         printf("> ");
         if (fgets(message, MAX, stdin) == NULL)
         {
-            // the user introduced Ctrl-D
+            // the user introduced Strg+D
             break;
         }
         else
